7e606272627f3376d84807e73431a8e72ad70d3c
svirom <svirom@yahoo.com>
2018-04-05 17:22:25 +0300

    fukasawa child

---
+++ wp-content/themes/fukasawa-child/page-deals.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Template Name: Deals Page
+*/
+include_once 'dbinc/dbdeals.php';
+
+get_header(); ?>
+
+<div class="content thin">
+
+		<?php
+
+			if (is_page('toys')) {
+				$sql = "SELECT * FROM wp_deals WHERE deal_category='toy';";
+			}
+			elseif (is_page('cups')) {
+				$sql = "SELECT * FROM wp_deals WHERE deal_category='cup';";
+			}
+			elseif (is_page('bells')) {
+				$sql = "SELECT * FROM wp_deals WHERE deal_category='bell';";
+			}
+			
+			$result = mysqli_query($conn, $sql);
+			$resultCheck = mysqli_num_rows($result);
+
+			if ($resultCheck > 0) { ?>
+				<div class="toy_wrapper"> <?php
+				while ($row = mysqli_fetch_assoc($result)) {
+					echo "<div class='col-3'>";
+					echo "<div class='item'>";
+					echo "<div class='img_wrap'><img src='". $row['deal_item_img'] ."' alt=''></div>";
+					echo "<h3>" . $row['deal_item_title'] . "</h3>";
+					echo "<p class='price_retail'>Retail price:<span>$" . $row['deal_price_high'] . "</span>(<a href='". $row['deal_url_high'] ."'>click to view</a>)</p>";
+					echo "<p class='price_deal'>Deal price:<span>$" . $row['deal_price_deal'] . "</span></p>";
+					echo "<a href='". $row['deal_url_deal'] ."'>View Deal</a>";
+					echo "</div>";
+					echo "</div>";
+				} ?>
+				</div>
+				<?php
+			}
+
+			?>			
+
+	
+	<div class="clear"></div>
+	
+</div><!-- .content -->
+								
+<?php get_footer(); ?>
